2d9c77f89d60032d612b7bc07768af802e068a56
purerosefallen <78877@qq.com>
2018-03-24 08:39:18 +0800

    others

--- ygopro-server.js
+++ ygopro-server.js
@@ -613,8 +613,8 @@
       } else if (name.slice(0, 3) === 'AI#') {
         this.hostinfo.rule = 2;
         this.hostinfo.lflist = -1;
-        this.hostinfo.time_limit = 0;
-        this.hostinfo.no_check_deck = true;
+        //this.hostinfo.time_limit = 0;
+        //this.hostinfo.no_check_deck = true;
       } else if ((param = name.match(/^(\d)(\d)(T|F)(T|F)(T|F)(\d+),(\d+),(\d+)/i))) {
         this.hostinfo.rule = parseInt(param[1]);
         this.hostinfo.mode = parseInt(param[2]);
@@ -1880,6 +1880,7 @@
     if (!room) {
       return;
     }
+    /* no kick in doom bot mode
     ref = room.players;
     for (j = 0, len = ref.length; j < len; j++) {
       player = ref[j];
@@ -1900,6 +1901,8 @@
       }
     }
     return false;
+    */
+    return true;
   });
 
   ygopro.stoc_follow('TYPE_CHANGE', false, function(buffer, info, client, server) {
